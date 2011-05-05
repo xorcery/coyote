3b98330fed9fb6bc0b1bb5764d7c45a0be802780
Casey O'Hara <caseyo@caseyohara.com>
2011-05-05 16:17:51 -0600

    Dependency discovery framed out but not fully implemented

--- lib/coyote/builder.rb
+++ lib/coyote/builder.rb
@@ -1,64 +1,98 @@
 module Coyote
   class Builder
 
-		attr_accessor :config, :options, :input_files
+		attr_accessor :config, :options, :input_files, :input_classes
 
 		def initialize(options)
 			@options = options
 			@config = get_config_or_screw_off
 			@input_files = []
+			@input_classes = []
 		end
-		
+
 
 		def build
+			@config['input'].each { |input| add_input input }
+			add_requires
 			output = Coyote::Output.new "#{@config['output']}"
-			
-			@config['input'].each do |key, value|
-				add_input_files key
-			end
-									
-			send_input_to_output output
+			send_files_to_output output
 			compress_and_save output
 		end
-		
-		def add_input_files(input)
+
+
+		def add_input(input)
 			if input.class == Array
 				input.each do |input|
-					@input_files.push input
+					find_and_add_files input
 				end
-			elsif input.class == String 
-				@input_files.push input
+			elsif input.class == String
+				find_and_add_files input
+			end
+		end
+
+		
+		def find_and_add_files(input)
+			Dir.glob(input).each do |file|
+				@input_files.push file
+				@input_classes.push file.gsub(/([.](js))/, '').gsub(/(["]|['])/, '').split('/').last
+			end
+		end
+
+		
+		def add_requires
+			@input_files.each do |file|
+				required = find_requires file
+				required.each do |requirement|
+					if ! @input_classes.include? requirement
+						find_and_add_files "**/#{requirement}.js"
+					end
+				end				
+			end
+		end
+
+		# move this into output and run at compile time
+		# so the file is only open once
+		# this will also make inserting the classes 
+		# in the right place easier		
+		def find_requires(input_file)
+			required = []
+			File.open(input_file) do |file|
+				file = file.read
+				pattern = Regexp.new(/(\/\/.*)(require )(.*)$/)
+				matches = file.scan(pattern)
+				matches.each do |match|
+					required.push match.last.strip.to_s.gsub(/([.](js))/, '').gsub(/(["]|['])/, '').split('/').last
+				end
+			end
+			return required
+		end	
+
+
+		def send_files_to_output(output)
+			@input_files.uniq.each do |file|
+				output.append file
 			end
 		end
 
 
-		def send_input_to_output(output)
-			@input_files.each do |input|
-				Dir.glob(input).each do |file|				
-					output.append file
-				end
-			end			
-		end
-		
-		
 		def compress_and_save(output)
 			if @options[:compress] || @config['compress']
 				output.compress
 			end
 
-			output.save		
-		end	
+			output.save
+		end
 
 
 		def get_config_or_screw_off
 			if File.exists?(Coyote::CONFIG_FILENAME)
 				begin
-					config = YAML.load(File.open(Coyote::CONFIG_FILENAME)) 
-					if config.class == Hash && ! config.empty?					
+					config = YAML.load(File.open(Coyote::CONFIG_FILENAME))
+					if config.class == Hash && ! config.empty?
 				  	return config
 					else
 						print "Coyote configuration exists but has not been defined yet. Configure it in #{Coyote::CONFIG_FILENAME}\n".red
-						exit(0)				
+						exit(0)
 					end
 				rescue ArgumentError => e
 				  print "Could not parse YAML: #{e.message}\n".red
@@ -69,8 +103,8 @@ module Coyote
 				exit
 			end
 		end
-		
-		
+
+
 
   end
 end
