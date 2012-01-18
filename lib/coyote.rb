ad5d64c7e21eb14654ee9c2de687d17eb75a82fb
Casey O'Hara <caseyo@caseyohara.com>
2012-01-18 10:40:01 -0700

    Version bump

--- lib/coyote.rb
+++ lib/coyote.rb
@@ -11,7 +11,7 @@ include Term::ANSIColor
 
 module Coyote
   APP_NAME        = "Coyote"
-  VERSION         = "0.6.1"
+  VERSION         = "0.7.0"
   ROOT_PATH       = Dir.pwd
   CONFIG_PATH     = File.expand_path(File.dirname(__FILE__) + "/../config")
   CONFIG_FILENAME = "coyote.yaml"
