0dbd96e146b0592eca057f1beef0020763753bae
Casey O'Hara <casey.ohara@imulus.com>
2012-05-26 02:43:01 -0600

    convert @bundle to a let() because I think it looks nicer

--- spec/coyote/bundle_spec.rb
+++ spec/coyote/bundle_spec.rb
@@ -1,49 +1,48 @@
 require 'coyote/bundle'
 
 describe Coyote::Bundle do
-  before :each do
-    @bundle = Coyote::Bundle.new      
-  end
+
+  let(:bundle) { Coyote::Bundle.new }
 
   context "#add" do
     it "adds an input to the bundle" do
-      @bundle.add("spec/assets/bundle/javascript/script1.js")
-      @bundle.assets.should have_key File.expand_path("spec/assets/bundle/javascript/script1.js")
+      bundle.add("spec/assets/bundle/javascript/script1.js")
+      bundle.assets.should have_key File.expand_path("spec/assets/bundle/javascript/script1.js")
     end
     
     it "adds an input's dependencies" do
-      @bundle.add("spec/assets/bundle/javascript/script3.js")      
-      @bundle.assets.should have_key File.expand_path("spec/assets/bundle/javascript/script3.js")
-      @bundle.assets.should have_key File.expand_path("spec/assets/bundle/javascript/script4.js")
+      bundle.add("spec/assets/bundle/javascript/script3.js")      
+      bundle.assets.should have_key File.expand_path("spec/assets/bundle/javascript/script3.js")
+      bundle.assets.should have_key File.expand_path("spec/assets/bundle/javascript/script4.js")
     end
     
     it "doesn't add the same dependency twice" do
-      @bundle.add("spec/assets/bundle/javascript/script1.js")
-      @bundle.add("spec/assets/bundle/javascript/script1.js")
-      @bundle.assets.length.should == 1
+      bundle.add("spec/assets/bundle/javascript/script1.js")
+      bundle.add("spec/assets/bundle/javascript/script1.js")
+      bundle.assets.length.should == 1
     end
   end
   
   context "#empty!" do
     it "removes all of its assets" do
-      @bundle.add("spec/assets/bundle/javascript/script1.js")
-      @bundle.empty!
-      @bundle.assets.should be_empty
+      bundle.add("spec/assets/bundle/javascript/script1.js")
+      bundle.empty!
+      bundle.assets.should be_empty
     end
     
     it "removes all of the bundle's contents" do
-      @bundle.add("spec/assets/bundle/javascript/script1.js")
-      @bundle.empty!
-      @bundle.contents.should be_empty
+      bundle.add("spec/assets/bundle/javascript/script1.js")
+      bundle.empty!
+      bundle.contents.should be_empty
     end
   end
 
   context "#contents" do
     it "returns the contents of all of its assets" do
-      @bundle.add("spec/assets/bundle/javascript/script1.js")
-      @bundle.add("spec/assets/bundle/javascript/script2.js")
+      bundle.add("spec/assets/bundle/javascript/script1.js")
+      bundle.add("spec/assets/bundle/javascript/script2.js")
       contents = IO.read("spec/assets/bundle/javascript/script1.js") + IO.read("spec/assets/bundle/javascript/script2.js")
-      @bundle.contents.should == contents
+      bundle.contents.should == contents
     end
   end
 
@@ -51,24 +50,24 @@ describe Coyote::Bundle do
     it "tells the changed files's assets to update" do
       input1 = "spec/assets/bundle/javascript/script1.js"
       input2 = "spec/assets/bundle/javascript/script2.js"
-      @bundle.add(input1)
-      @bundle.add(input2)
-      @bundle.assets[File.expand_path(input1)].should_receive(:update!)
-      @bundle.assets[File.expand_path(input2)].should_receive(:update!)
-      @bundle.update!([File.expand_path(input1),File.expand_path(input2)])
+      bundle.add(input1)
+      bundle.add(input2)
+      bundle.assets[File.expand_path(input1)].should_receive(:update!)
+      bundle.assets[File.expand_path(input2)].should_receive(:update!)
+      bundle.update!([File.expand_path(input1),File.expand_path(input2)])
     end
 
     it "resets the cache" do
-      @bundle.should_receive(:reset!)
-      @bundle.update!
+      bundle.should_receive(:reset!)
+      bundle.update!
     end    
   end
 
   context "#reset!" do
     it "sets the @contents ivar to nil to reset the memoization in #contents" do
-      @bundle.add("spec/assets/bundle/javascript/script1.js")
-      @bundle.reset!
-      @bundle.instance_variable_get(:@contents).should be_nil
+      bundle.add("spec/assets/bundle/javascript/script1.js")
+      bundle.reset!
+      bundle.instance_variable_get(:@contents).should be_nil
     end
   end
   
