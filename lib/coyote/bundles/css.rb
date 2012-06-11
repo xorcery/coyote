cf0fc50f9097306557d5212a470952f9d00a7105
Casey O'Hara <casey.ohara@imulus.com>
2012-06-10 18:41:14 -0600

    Add CSS compressor

--- lib/coyote/bundles/css.rb
+++ lib/coyote/bundles/css.rb
@@ -3,6 +3,11 @@ module Coyote::Bundles
     filetypes :css, :less
 
     def compress!
+      tmp = "#{@target}.#{Time.now.to_i}.less"
+      File.open(tmp, 'w+') { |f| f.write contents }
+      self.contents = `lessc #{tmp} -x`
+      File.delete tmp
     end
   end
 end
+
