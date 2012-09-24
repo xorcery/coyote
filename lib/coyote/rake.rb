d81d22ff82ebb099c8af2e26f303af50c9fce321
Casey O'Hara <casey.ohara@imulus.com>
2012-09-24 12:31:52 -0600

    Add :compress as a default provided task

--- lib/coyote/rake.rb
+++ lib/coyote/rake.rb
@@ -2,19 +2,26 @@ require 'coyote/dsl'
 
 def coyote(*args, &block)
   runner = Coyote::DSL.new(&block)
-  
+
   args ||= []
 
   args.insert 0, :build
   Rake::Task.define_task(*args) do
     runner.options[:watch] = false
-    runner.run    
+    runner.run
   end
 
   args[0] = :watch
   Rake::Task.define_task(*args) do
     runner.options[:watch] = true
-    runner.run    
-  end   
-   
+    runner.run
+  end
+
+  args[0] = :compress
+  Rake::Task.define_task(*args) do
+    runner.options[:compress] = true
+    runner.options[:watch] = false
+    runner.run
+  end
+
 end
